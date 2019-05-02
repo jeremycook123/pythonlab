8fceede3071dcb20016cfe392b41189d292e0fa9
Jeremy Cook <jeremy.cook@cloudacademy.com>
2019-05-02 17:28:33 +1200

    added lp1 and l1

---
+++ lp1/l1/code/HelloWorld/lab-code/hello_world.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+import sys
+sys.version_info[0]
+
+lab_exercise = "HelloWorld"
+lab_type = "lab-code"
+python_version = ("%s.%s.%s" % (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
+
+print("Exercise: %s" % (lab_exercise))
+print("Type: %s" % (lab_type))
+print("Python: %s\n" % (python_version))
+
+#====================================
+
+#CODE1: Declare message variables
+
+#CODE2: Use string concaternation then print to the console
+
+#CODE3: Use string formatting and interpolation then print to the console
+
+#CODE4: Uppercase the message vars before printing to the console:
+
+#CODE5: Create an array of words and then loop over array and determine string length of each word
---
+++ lp1/l1/code/Traversal/lab-code/walk_ex.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+import sys
+sys.version_info[0]
+
+lab_exercise = "Traversal"
+lab_type = "lab-code"
+python_version = ("%s.%s.%s" % (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
+print("Exercise: %s" % (lab_exercise))
+print("Type: %s" % (lab_type))
+print("Python: %s\n" % (python_version))
+
+#====================================
+
+#CODE1: Import the os module
+
+#CODE2: Start in parent directory
+
+#CODE3: Perform a directory traversal starting in the parent directory
+
+#CODE4: If main then call the traversal function
